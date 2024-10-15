c20898e2c97d3146425857402a5776b5dea7a697
giuse-pio <giupeppepio7@gmail.com>
2024-10-15 18:16:15 +0200

    con il ciclo for

--- main.c
+++ main.c
@@ -9,9 +9,13 @@ int main(void) {
     printf(" inserisci i valori di num:");
     scanf("%d", &num);
     int i = a;
-    while (i >= a && i <= b) {
+    /*while (i >= a && i <= b) {
         printf("i multipli di num sono: %d\n" ,num * i);
         i++;
+
+    }*/
+    for (i = a; i <= b; i++) {
+        printf("i multipli di num sono: %d\n" ,num * i);
     }
     return 0;
 }
